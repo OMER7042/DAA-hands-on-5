c137fbe03473dc06cace024e1785e1ce12fdb2d7
MD OMER AHMED <105853902+OMER7042@users.noreply.github.com>
2024-02-17 20:10:21 -0600

    Update Min_heap.py

--- Min_heap.py
+++ Min_heap.py
@@ -1,3 +1,12 @@
+# A min heap is a binary tree where each node is less than or equal to its children, making the smallest element the root.
+# The MinHeap class uses an array to represent the heap structure.
+# "get_minimum" returns the smallest element (root) in the heap.
+# "pop_minimum" removes and returns the smallest element while preserving the heap property.
+# "insert_value" adds a new element while maintaining the heap property.
+# I have also used float values with integers as the input to show that it supports various data types.
+# I have executed the same code for custom data structues in the file named "minheap_custom_ds.py".
+
+
 class MinHeap:
     def __init__(self):
         self.heap = []
@@ -70,7 +79,7 @@ class MinHeap:
 # Lets implement an example for all the functions we have included.
             
 heap = MinHeap()
-heap.build_min_heap([12, 16, 4, 1, 8, 15])
+heap.build_min_heap([12, 16, 4, 1,1.2, 8, 15, 14.1])
 print("Min Heap is:", heap.heap) 
 
 #insertion of an element into the heap
@@ -85,3 +94,16 @@ print("Popped Minimum value:", heap.pop_minimum())
 
 #print back the updated heap
 print("Heap after Popping the minimum value:", heap.heap)  
+
+
+
+###OUTPUT 
+C:\Users\OMER>C:/Python311/python.exe "c:/Users/OMER/Desktop/DAA codes/minheap.py"
+Min Heap is: [1, 1.2, 4, 14.1, 12, 8, 15, 16]
+After Inserting 9 : [1, 1.2, 4, 9, 12, 8, 15, 16, 14.1]
+Minimum Value: 1
+Popped Minimum value: 1
+Heap after Popping the minimum value: [1.2, 9, 4, 14.1, 12, 8, 15, 16]
+
+
+
